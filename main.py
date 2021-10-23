90e4bd55b6d5ddabd5137583156183b44c455c0f
SantiagoMunz <467087121@qq.com>
2021-10-23 13:41:37 +0800

    1023

--- main.py
+++ main.py
@@ -30,43 +30,33 @@ def main(path2CFile, path2CFG, path2SMT):
             print("Loop invariant Inference is OOT")
             return None,None
         Iteration += 1
-        print("Iteration:   ", Iteration)
-        print("Size of CE:   ", len(CE['p']), len(CE['n']), len(CE['i']))
         # Step 3.1 Generate A partial template
         PT = pT_generator.generate_next(CE)
         if PT is None:
             print("The only way is to give up now")
             return None,None
-        print('PT: ', PT)
         # Step 3.2 Solving the partial template
         try:
             Can_I = Template_solver.solve(PT, CE)
-            print('CanI: ', Can_I)
             #raise TimeoutError # try this thing out
         except TimeoutError as OOT:  # Out Of Time, we punish
             pT_generator.punish('STRICT', 'VERY', 'S')
-            print('Template Solving is OOT')
             continue
         if Can_I is None:  # Specified too much, we loose.
             pT_generator.punish('LOOSE', 'MEDIUM', 'S')
-            print('Template Solving is Failed')
             continue
         # Step 3.3 Check if we bingo
         try:
             Counter_example = sMT_verifier.verify(Can_I, path2SMT)
-            if Counter_example is not None:
-                print('Counter_example: ', Counter_example.kind, Counter_example.assignment)
-            else:
-                print('Counter_example: None')
         except TimeoutError as OOT:  # Out Of Time, we punish
             pT_generator.punish('STRICT', 'LITTLE', 'V')
-            print('SMT checking is OOT')
             continue
         if Counter_example is None:  # Bingo, we prise
             solved = True
             print("The answer is :  ", str(Can_I))
             pT_generator.prise('VERY')
             current_time = time.time()
+            print("Time cost is :  ", str(current_time - start_time))
             return current_time - start_time, str(Can_I)
         else:  # progressed anyway, we prise
             if Counter_example.assignment not in CE[Counter_example.kind]:
